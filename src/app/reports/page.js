1b5cc34b768d3e93b04f50dc6d24033f00e479f3
zachranymail123-a11y <zachranymail123@gmail.com>
2026-02-25 00:57:08 +0100

    Update page.js

--- src/app/reports/page.js
+++ src/app/reports/page.js
@@ -4,8 +4,8 @@ export const dynamic = 'force-dynamic'; // Chceme vždy čerstvá data
 
 export default async function ReportsPage() {
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
+    process.env.https://luepzmdwgrbtnevlznbx.supabase.co,
+    process.env.sb_publishable_wa3MgO-wdn8oWrZbJReNPw_CT9Bp2mq
   );
 
   // Vytáhneme všechny reporty seřazené od nejnovějšího
