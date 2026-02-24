850764115177ffab6e844364f45d0895992e3ff3
zachranymail123-a11y <zachranymail123@gmail.com>
2026-02-24 19:55:02 +0100

    Delete src/app/api/cron/route.js

--- src/app/api/cron/route.js
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-dynamic';
-
-export async function GET() {
-  return new Response(JSON.stringify({ message: "KONECNE TO ŽIJE" }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
