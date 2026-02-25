ae57204bbfd094e3dada06f4b72cb1254c898816
zachranymail123-a11y <zachranymail123@gmail.com>
2026-02-25 07:50:15 +0100

    Update robots.js

--- src/app/robots.js
+++ src/app/robots.js
@@ -3,7 +3,6 @@ export default function robots() {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/api/', // Skryjeme API před Googlem
     },
     sitemap: 'https://www.thehardwareguru.cz/sitemap.xml',
   }
