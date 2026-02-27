dc6ddbb1851f2aea752fba8400d2232ac27be13d
zachranymail123-a11y <zachranymail123@gmail.com>
2026-02-27 13:32:55 +0100

    Update page.js

--- src/app/sestavy/page.js
+++ src/app/sestavy/page.js
@@ -1,3 +1,7 @@
+export const revalidate = 0;
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
 import { createClient } from '@supabase/supabase-js';
 import Link from 'next/link';
 
