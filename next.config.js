5dbe6b39ed6e8bc27a9f2d253bd22e40d24c3f13
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-01 23:47:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,7 +18,19 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com', // Tvoje záložní fotka z page.js
+        hostname: 'i.ytimg.com', // YouTube náhledy (pro live streamy) [cite: 2026-02-25]
+      },
+      {
+        protocol: 'https',
+        hostname: 'kick.com', // Hlavní doména Kicku [cite: 2026-02-28]
+      },
+      {
+        protocol: 'https',
+        hostname: '*.kick.com', // Subdomény Kicku pro obrázky [cite: 2026-02-28]
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com', // Záložní fotka
       }
     ],
   },
