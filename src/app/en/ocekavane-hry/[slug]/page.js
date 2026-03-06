49cdc352a826a239634dcf76215d9d944f853d4a
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-06 18:20:27 +0100

    Update page.js

--- src/app/en/ocekavane-hry/[slug]/page.js
+++ src/app/en/ocekavane-hry/[slug]/page.js
@@ -1,7 +1,7 @@
 /**
  * GURU MASTER PROXY: Detail Očekávané Hry (Slug) (EN)
- * GURU FIX: Přímý re-export řeší chybu "Unsupported Server Component type" na Vercelu pro anglickou mutaci detailu.
- * * Tento soubor aktivuje cestu /en/ocekavane-hry/[slug], zatímco veškerou logiku, 
- * databázové dotazy a elitní design obstarává hlavní soubor v src/app/ocekavane-hry/[slug]/page.js.
+ * GURU FIX: Použití absolutního aliasu (@/app) řeší chybu "Module not found" na Vercelu.
+ * Tento soubor aktivuje cestu /en/ocekavane-hry/[slug], zatímco veškerou logiku 
+ * a elitní design obstarává hlavní soubor v src/app/ocekavane-hry/[slug]/page.js.
  */
-export { default } from '../../../ocekavane-hry/[slug]/page';
+export { default } from '@/app/ocekavane-hry/[slug]/page';
