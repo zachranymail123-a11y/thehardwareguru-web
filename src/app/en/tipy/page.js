4c78db5ff143f1d4bc8cc4879271c7e1f6dde37a
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-06 01:27:43 +0100

    Update page.js

--- src/app/en/tipy/page.js
+++ src/app/en/tipy/page.js
@@ -1,12 +1,6 @@
-"use client";
-
 /**
- * GURU EN PROXY: Tipy
- * Tento soubor zajišťuje, že adresa /en/tipy funguje.
- * Načítá hlavní komponentu, která díky usePathname() automaticky přepne texty do angličtiny.
+ * GURU MASTER PROXY: Anglické Tipy
+ * GURU FIX: Přímý re-export řeší chybu "Unsupported Server Component type".
+ * Tento pattern zajišťuje maximální stabilitu Next.js kompilátoru během produkčního buildu.
  */
-import TipyArchivePage from '../../tipy/page';
-
-export default function EnglishTipsPage() {
-  return <TipyArchivePage />;
-}
+export { default } from '../../tipy/page';
