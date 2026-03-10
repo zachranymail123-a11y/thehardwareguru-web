f8ba6fcbb8583fba2b824c71c61f9ad95a323a39
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-10 23:03:53 +0100

    Update page.js

--- src/app/en/cpu-fps/[slug]/[game]/page.js
+++ src/app/en/cpu-fps/[slug]/[game]/page.js
@@ -2,4 +2,4 @@
  * GURU MASTER PROXY: CPU FPS Benchmark EN
  * Zajišťuje existenci cesty /en/cpu-fps/[cpu]/[game]
  */
-export { default, generateMetadata } from '../../../cpu-fps/[cpu]/[game]/page';
+export { default, generateMetadata } from '../../../cpu-fps/[slug]/[game]/page';
