58eb9a285d71ff9e0fa066460ebe89e318f0f738
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-10 21:38:08 +0100

    Delete src/app/en/cpu-fps/[cpu]/[game] directory

--- src/app/en/cpu-fps/[cpu]/[game]/page.js
+++
@@ -1,5 +0,0 @@
-/**
- * GURU MASTER PROXY: CPU FPS Benchmark EN
- * Zajišťuje existenci cesty /en/cpu-fps/[cpu]/[game]
- */
-export { default, generateMetadata } from '../../../cpu-fps/[cpu]/[game]/page';
