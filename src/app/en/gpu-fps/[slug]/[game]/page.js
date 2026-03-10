09f9e9a828ee7e2a1512e72dfdc426786c98b4f7
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-10 23:03:24 +0100

    Update page.js

--- src/app/en/gpu-fps/[slug]/[game]/page.js
+++ src/app/en/gpu-fps/[slug]/[game]/page.js
@@ -2,4 +2,4 @@
  * GURU MASTER PROXY: Detail herního FPS
  * Tento soubor zajišťuje existenci cesty /en/gpu-fps/[gpu]/[game]
  */
-export { default, generateMetadata } from '../../../../gpu-fps/[gpu]/[game]/page';
+export { default, generateMetadata } from '../../../../gpu-fps/[slug]/[game]/page';
