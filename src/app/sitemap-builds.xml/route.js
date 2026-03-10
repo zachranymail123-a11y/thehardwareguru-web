27372e02162a40bd4b037673d6a6a9aa6e0520dc
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-10 02:15:25 +0100

    Create route.js

---
+++ src/app/sitemap-builds.xml/route.js
@@ -0,0 +1,32 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const revalidate=3600;
+
+export async function GET(){
+
+const supabase=createClient(
+process.env.NEXT_PUBLIC_SUPABASE_URL,
+process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
+const base="https://thehardwareguru.cz";
+
+const {data}=await supabase
+.from("builds")
+.select("slug");
+
+let urls="";
+
+data?.forEach(b=>{
+
+urls+=`<url><loc>${base}/sestavy/${b.slug}</loc></url>`;
+
+});
+
+const xml=`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+return new Response(xml,{headers:{'Content-Type':'application/xml'}});
+}
