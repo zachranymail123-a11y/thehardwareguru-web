5904819d032a7d11db5b6b396f49ce2b605fa090
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-10 01:43:43 +0100

    Update route.js

--- src/app/sitemap-index.xml/route.js
+++ src/app/sitemap-index.xml/route.js
@@ -1,28 +1,38 @@
+export const revalidate = 3600;
+
 export async function GET() {
 
-  const baseUrl = "https://thehardwareguru.cz";
+const base="https://thehardwareguru.cz";
 
-  const sitemaps = [
-    `${baseUrl}/sitemap-pages.xml`,
-    `${baseUrl}/sitemap-gpus.xml`,
-    `${baseUrl}/sitemap-benchmarks.xml`,
-    `${baseUrl}/sitemap-duels.xml`,
-    `${baseUrl}/sitemap-upgrades.xml`
-  ];
+const maps=[
+"sitemap-pages.xml",
+"sitemap-posts.xml",
+"sitemap-tipy.xml",
+"sitemap-tweaky.xml",
+"sitemap-rady.xml",
+"sitemap-slovnik.xml",
+"sitemap-gpus.xml",
+"sitemap-gpu-recommend.xml",
+"sitemap-upgrades.xml",
+"sitemap-gpu-duels.xml",
+"sitemap-cpu-duels.xml",
+"sitemap-benchmarks.xml",
+"sitemap-duel-benchmarks.xml",
+"sitemap-gpu-fps.xml",
+"sitemap-best-gpu.xml",
+"sitemap-calendar.xml",
+"sitemap-builds.xml"
+];
 
-  const body =
-`<?xml version="1.0" encoding="UTF-8"?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${sitemaps.map((url)=>`
+const urls=maps.map(m=>`
 <sitemap>
-<loc>${url}</loc>
-</sitemap>`).join("")}
+<loc>${base}/${m}</loc>
+</sitemap>`).join("");
+
+const xml=`<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
 </sitemapindex>`;
 
-  return new Response(body,{
-    headers:{
-      "Content-Type":"application/xml"
-    }
-  });
-
+return new Response(xml,{headers:{'Content-Type':'application/xml'}});
 }
