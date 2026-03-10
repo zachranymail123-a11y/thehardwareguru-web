310fe3227d51a0fad6b4f858105099458379fb06
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-10 02:01:16 +0100

    Create page.js

---
+++ src/app/sitemap-tweaky.xml/page.js
@@ -0,0 +1,36 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const revalidate = 3600;
+
+export async function GET() {
+
+const supabase = createClient(
+process.env.NEXT_PUBLIC_SUPABASE_URL,
+process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
+const base = "https://thehardwareguru.cz";
+
+const { data } = await supabase
+.from("tweaky")
+.select("slug, slug_en");
+
+let urls="";
+
+data?.forEach(t=>{
+
+urls+=`<url><loc>${base}/tweaky/${t.slug}</loc></url>`;
+
+if(t.slug_en){
+urls+=`<url><loc>${base}/en/tweaky/${t.slug_en}</loc></url>`;
+}
+
+});
+
+const xml=`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+return new Response(xml,{headers:{'Content-Type':'application/xml'}});
+}
