539eceeacf8e2ee593daa2cdf1fce9faef2b7b75
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-10 04:04:59 +0100

    Update route.js

--- src/app/sitemap-builds.xml/route.js
+++ src/app/sitemap-builds.xml/route.js
@@ -7,7 +7,7 @@ export async function GET(){
 
 const supabase = createClient(
 process.env.NEXT_PUBLIC_SUPABASE_URL,
-process.env.SUPABASE_SERVICE_ROLE_KEY
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 const base = "https://thehardwareguru.cz";
@@ -41,5 +41,4 @@ ${urls}
 return new Response(xml,{
 headers:{'Content-Type':'application/xml'}
 });
-
 }
