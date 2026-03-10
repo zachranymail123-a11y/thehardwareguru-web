5b5dcc855301d09c98389a461dee38c0739caacf
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-10 20:11:22 +0100

    Delete src/app/en directory

--- src/app/en/clanky/[slug]/page.js
+++
@@ -1,6 +0,0 @@
-/**
- * GURU MASTER PROXY: Detail Článku (Slug)
- * GURU FIX: Přímý re-export řeší chybu "Unsupported Server Component type" při produkčním buildu.
- * Tento soubor zajišťuje existenci cesty /en/clanky/[slug] a plnou funkčnost detailu.
- */
-export { default } from '../../../clanky/[slug]/page';
--- src/app/en/sin-slavy/page.js
+++
@@ -1,4 +0,0 @@
-/**
- * GURU MASTER PROXY: Síň Slávy (EN)
- */
-export { default } from '../../sin-slavy/page';
