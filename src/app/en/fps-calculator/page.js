b3dc8be8fddd2789fb9421654c44650e5df2e4ff
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-16 19:41:44 +0100

    Create page.js

---
+++ src/app/en/fps-calculator/page.js
@@ -0,0 +1,7 @@
+/**
+ * GURU FPS ENGINE - EN PROXY
+ * Přesměrovává anglickou URL na hlavní komponentu s českou logikou,
+ * která si díky props (isEn) sama přepne jazyk.
+ */
+
+export { default, generateMetadata } from '../../fps-kalkulacka/page';
