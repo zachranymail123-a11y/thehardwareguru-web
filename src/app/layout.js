ab0a2009c03de330f6dde2da1e3b62b236605448
zachranymail123-a11y <zachranymail123@gmail.com>
2026-03-21 00:22:46 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -76,14 +76,6 @@ export default async function RootLayout({ children, params }) {
   return (
     <html lang={locale}>
       <head>
-        {/* --- EZOIC INTEGRACE --- */}
-        <script data-cfasync="false" src="https://cmp.gatekeeperconsent.com/min.js"></script>
-        <script data-cfasync="false" src="https://the.gatekeeperconsent.com/cmp.min.js"></script>
-        <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
-        <script dangerouslySetInnerHTML={{ __html: `window.ezstandalone = window.ezstandalone || {}; ezstandalone.cmd = ezstandalone.cmd || [];` }} />
-        <script src="//ezoicanalytics.com/analytics.js"></script>
-        {/* ----------------------- */}
-
         {/* 🚀 GOOGLE ADSENSE GLOBAL TAG (Přesně podle tvého screenshotu) */}
         <script 
           async 
