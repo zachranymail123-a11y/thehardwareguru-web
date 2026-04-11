d1a202695413ad155ba162bca9a11c10740781c5
zachranymail123-a11y <zachranymail123@gmail.com>
2026-04-11 04:49:02 +0200

    Update page.js

--- src/app/en/fps-calculator/page.js
+++ src/app/en/fps-calculator/page.js
@@ -1,7 +1,17 @@
+import FpsKalkulackaPage, { generateMetadata as originalMetadata } from '../../fps-kalkulacka/page';
+
 /**
- * GURU FPS ENGINE - EN PROXY
- * Přesměrovává anglickou URL na hlavní komponentu s českou logikou,
- * která si díky props (isEn) sama přepne jazyk.
+ * GURU FPS ENGINE - EN PROXY (FIXED)
+ * Explicitně balí originální stránku a natvrdo jí předává informaci, že je v EN.
  */
 
-export { default, generateMetadata } from '../../fps-kalkulacka/page';
+export async function generateMetadata(props) {
+    const p = await props.params;
+    return originalMetadata({ ...props, params: { ...p, lang: 'en' } });
+}
+
+export default async function EnFpsCalculatorProxy(props) {
+    const p = await props.params;
+    // Předáváme params i speciální prop přímo do hlavní komponenty
+    return <FpsKalkulackaPage {...props} params={{ ...p, lang: 'en' }} isEnProxy={true} />;
+}
